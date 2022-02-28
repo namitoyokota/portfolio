3760067dbfa5af6beca7a09999bdc6702e9bd2b9
Namito Yokota <nyokota@crimson.ua.edu>
2022-02-28 11:39:05 -0600

    added content to about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,8 +8,10 @@ export const About = (): JSX.Element => {
         title: 'About - Namito Yokota',
       }}
     >
-      <h1>About Page</h1>
-      <p>Welcome to the about page</p>
+      <iframe src="https://open.spotify.com/embed/track/5G2g7titQVbMK8jjRPWWr6?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/jG7dSXcfVqE?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/DSgJ1sejWtw?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/XiCrniLQGYc?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </Layout>
   );
 };
