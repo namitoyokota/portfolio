6916da7c21a678dea281f8786c6675e04ad3e522
Namito Yokota <nyokota@crimson.ua.edu>
2022-02-28 11:38:37 -0600

    add active flag to navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,15 +1,22 @@
 import Link from 'next/link';
 import React from 'react';
 import styles from '../styles/Home.module.css';
+import { useRouter } from "next/router";
 
 const Navigation = (): JSX.Element => {
+  const router = useRouter();
+
   return (
     <>
-      <Link href="/">
-        <a className={styles.page}>Home</a>
+      <Link href="/" >
+        <a className={router.asPath == "/" ? styles.activepage : styles.page}>
+          <h4>Home</h4>
+        </a>
       </Link>
       <Link href="/about">
-        <a className={styles.page}>About</a>
+        <a className={router.asPath == "/about" ? styles.activepage : styles.page}>
+          <h4>About</h4>
+        </a>
       </Link>
     </>
   );
