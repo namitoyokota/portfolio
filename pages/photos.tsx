c80514efde849786d3786fd7e3dc4ec7c20a9c88
Namito Yokota <nyokota@crimson.ua.edu>
2022-03-01 17:02:33 -0600

    fixed alt and some names of image files

--- pages/photos.tsx
+++ pages/photos.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import styles from '../styles/Home.module.css';
+import Image from 'next/image';
 
 export const About = (): JSX.Element => {
   return (
@@ -12,20 +13,20 @@ export const About = (): JSX.Element => {
 
       <div className={styles.imagerow}>
         <div className={styles.imagecolumn}>
-          <img src="/photos/grad-pic-hannah.jpg" />
-          <img src="/photos/worship-1.jpg" />
-          <img src="/photos/japan-trip-1.jpg" />
-          <img src="/photos/japan-trip-3.jpg" />
-          <img src="/photos/sky-diving-11.jpg" />
-          <img src="/photos/vlog-squad-12.jpg" />
+          <img src="/photos/grad-pic-hannah.jpg" alt="Hannah Graduation Picture" />
+          <img src="/photos/worship-1.jpg" alt="Worship Picture" />
+          <img src="/photos/japan-trip-1.jpg" alt="Japan Trip" />
+          <img src="/photos/japan-trip-3.jpg" alt="Japan Trip" />
+          <img src="/photos/sky-diving-1.jpg" alt="Sky Diving" />
+          <img src="/photos/bryant-denny-1.jpg" alt="Bryant Denny Friends" />
         </div>
         <div className={styles.imagecolumn}>
-          <img src="/photos/grad-pic-hannah-haylee.jpg" />
-          <img src="/photos/grad-pic-haylee.jpg" />
-          <img src="/photos/worship-2.jpg" />
-          <img src="/photos/japan-trip-2.jpg" />
-          <img src="/photos/sky-diving-12.jpg" />
-          <img src="/photos/haloween-1.jpg" />
+          <img src="/photos/grad-pic-hannah-haylee.jpg" alt="Hannah and Haylee Graduation Picture" />
+          <img src="/photos/grad-pic-haylee.jpg" alt="Haylee Graduation Picture" />
+          <img src="/photos/worship-2.jpg" alt="Worship" />
+          <img src="/photos/japan-trip-2.jpg" alt="Japan Trip" />
+          <img src="/photos/sky-diving-2.jpg" alt="Sky Diving" />
+          <img src="/photos/halloween-1.jpg" alt="Halloween" />
         </div>
       </div>
     </Layout>
