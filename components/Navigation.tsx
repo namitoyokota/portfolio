9f7dff364e67c413680b4abb061557ec369658d5
Namito Yokota <nyokota@crimson.ua.edu>
2022-04-04 15:18:12 -0500

    build fix for on click sound effect

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -11,22 +11,32 @@ const Navigation = (): JSX.Element => {
   return (
     <>
       <Link href="/">
-        <a className={router.asPath == "/" ? styles.activepage : styles.page} onClick={play}>
+        <a className={router.asPath == "/" ? styles.activepage : styles.page}
+          onClick={() => play()}
+        >
           <h4>Home</h4>
         </a>
       </Link>
       <Link href="/dashboard">
-        <a className={router.asPath == "/dashboard" ? styles.activepage : styles.page} onClick={play}>
+        <a className={router.asPath == "/dashboard" ? styles.activepage : styles.page}
+          onClick={() => play()}
+        >
           <h4>Dashboard</h4>
         </a>
       </Link>
       <Link href="/jesus">
-        <a className={router.asPath == "/jesus" ? styles.activepage : styles.page} onClick={play}>
+        <a className={router.asPath == "/jesus" ? styles.activepage : styles.page}
+          onClick={() => play()}
+        >
           <h4>Jesus</h4>
         </a>
       </Link>
       <Link href="https://www.namito.wiki" passHref>
-        <a className={styles.page} target="_blank" rel="noopener noreferrer" onClick={play}>
+        <a className={styles.page}
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={() => play()}
+        >
           <h4>Search</h4>
         </a>
       </Link>
