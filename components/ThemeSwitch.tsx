8aa08f309f8b0cf11add634ebfffd55ff90e4d1a
Namito Yokota <nyokota@crimson.ua.edu>
2022-04-25 13:30:55 -0500

    cleaned up theme switch component

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -5,13 +5,15 @@ import styles from '../styles/Home.module.css';
 import useSound from 'use-sound';
 
 const ThemeSwitch = (): JSX.Element => {
-  const [mounted, setMounted] = React.useState(true);
+
+  /** Sets dark mode or light mode theme */
   const { theme, setTheme } = useTheme();
-  const [play] = useSound('/sound/stop.mp3');
 
-  React.useEffect(() => setMounted(true), []);
-
+  /** Indicates current state of theme */
   const isDark = theme === 'dark';
+
+  /** Plays click sound on button press */
+  const [play] = useSound('/sound/stop.mp3');
 
   return (
     <Image
