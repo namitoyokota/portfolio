afb91602debb798328133e3f80949f0f150b4c44
Namito Yokota <nyokota@crimson.ua.edu>
2022-05-16 10:19:51 -0500

    set default theme to light

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="light">
       <Component {...pageProps} />
     </ThemeProvider>
   );
