108b162fa1509d69fe984f88d83a4405146ebba2
Namito Yokota <namitoyokota@icloud.com>
2023-03-19 20:47:09 -0500

    cleaned up colors

--- utils/mdxUtils.ts
+++ utils/mdxUtils.ts
@@ -2,6 +2,4 @@ import fs from 'fs';
 import path from 'path';
 
 export const POSTS_PATH = path.join(process.cwd(), 'posts');
-export const postFilePaths = fs
-  .readdirSync(POSTS_PATH)
-  .filter((path) => /\.mdx?$/.test(path));
+export const postFilePaths = fs.readdirSync(POSTS_PATH).filter((path) => /\.mdx?$/.test(path));
