7aaf95cb369416715aebb7a52529b538e6dc8a95
Namito Yokota <namitoyokota@icloud.com>
2023-03-21 10:30:24 -0500

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
@@ -6,6 +7,7 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
     return (
         <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
             <Component {...pageProps} />
+            <Analytics />
         </ThemeProvider>
     );
 };
