6219b1ce9b76efd385798ba935cdce841d030c25
Namito Yokota <namitoyokota@icloud.com>
2024-02-28 15:45:45 -0600

    added rough notation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
+import { useEffect } from 'react';
+import { annotate, annotationGroup } from 'rough-notation';
 import Layout from '../components/Layout';
 import styles from '../styles/index.module.css';
 
 export const Index = (): JSX.Element => {
+    /**
+     * Adds rough notations to the page
+     */
+    useEffect(() => {
+        const frontendDeveloperText = annotate(document.querySelector('#frontend-developer') as HTMLElement, { type: 'underline' });
+        const venminderText = annotate(document.querySelector('#venminder') as HTMLElement, { type: 'box' });
+
+        annotationGroup([frontendDeveloperText, venminderText]).show();
+    }, []);
+
     return (
         <Layout>
             <div className={styles.pane}>
@@ -9,7 +21,9 @@ export const Index = (): JSX.Element => {
                     <h1>
                         hey, i'm namito <span className="animate__animated animate__bounce">👋🏼</span>
                     </h1>
-                    <span>I'm a Frontend Developer at Venminder.</span>
+                    <span>
+                        I'm a <span id="frontend-developer">Frontend Developer</span> at <span id="venminder">Venminder</span>.
+                    </span>
                 </div>
 
                 <p>this is my identity page. write about who i am, what i like to do, why i'm here, etc.</p>
