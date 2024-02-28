1397661a0793ecb97be363355d053257260cca87
Namito Yokota <namitoyokota@icloud.com>
2024-02-28 15:33:21 -0600

    added animate.css

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,10 @@ export const Index = (): JSX.Element => {
         <Layout>
             <div className={styles.pane}>
                 <div className={styles.header}>
-                    <h1>hi, i'm namito 👋🏼</h1>
-                    <span>I'm a Senior Frontend Engineer II at Venminder.</span>
+                    <h1>
+                        hey, i'm namito <span className="animate__animated animate__bounce">👋🏼</span>
+                    </h1>
+                    <span>I'm a Frontend Developer at Venminder.</span>
                 </div>
 
                 <p>this is my identity page. write about who i am, what i like to do, why i'm here, etc.</p>
