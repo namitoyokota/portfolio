16c2675aea751aba61e1bc8ef6b68cde0e2a120a
Namito Yokota <namitoyokota@icloud.com>
2024-02-28 16:07:25 -0600

    my story

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,16 +19,25 @@ export const Index = (): JSX.Element => {
     return (
         <Layout>
             <div className={styles.pane}>
+                {/* first impression */}
                 <div className={styles.header}>
                     <h1>
                         hey, i'm namito <span className="animate__animated animate__bounce">👋🏼</span>
                     </h1>
                     <span>
-                        I'm a <span id="frontend-developer">Frontend Developer</span> who loves to write clean and pixel perfect code.
+                        i'm a <span id="frontend-developer">frontend developer</span> who loves to write clean and pixel perfect code.
                     </span>
                 </div>
 
-                <p>this is my identity page. write about who i am, what i like to do, why i'm here, etc.</p>
+                {/* my story */}
+                <p>
+                    when i was in <u>9th grade</u>, a business class i wanted to sign up for was full. my counselor told me that i can
+                    either take "coding" or "piano" class. ever since i made the obvious choice, i've been in love with the{' '}
+                    <u>freedom and creativity</u> of coding!
+                </p>
+
+                {/* action step */}
+                <span>i'm open to senior frontend developer position at a passionate and team-oriented organization!</span>
             </div>
         </Layout>
     );
