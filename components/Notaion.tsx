cf82d326b175a003545304ffd0681436e87b71cb
Namito Yokota <namitoyokota@icloud.com>
2024-03-05 16:45:26 -0600

    hide notation when not in view

--- components/Notaion.tsx
+++ components/Notaion.tsx
@@ -4,21 +4,18 @@ import { NotationProps } from '../models/types/notation-props';
 
 const Notation = ({ children, config }: NotationProps): JSX.Element => {
     /** Reference to the element to annotate */
-    const ref = useRef<HTMLSpanElement>(null);
+    const ref = useRef<HTMLDivElement>(null);
 
     /** Annotates when element is in view */
     useEffect(() => {
+        const notation = annotate(ref.current, config);
         const observer = new IntersectionObserver(([entry]) => {
-            if (entry.isIntersecting) {
-                setTimeout(() => {
-                    annotate(entry.target as HTMLElement, config).show();
-                }, 750);
-            }
+            entry.isIntersecting ? notation.show() : notation.hide();
         });
 
         observer.observe(ref.current);
         return () => observer.disconnect();
-    }, []);
+    }, [children, config]);
 
     return <span ref={ref}>{children}</span>;
 };
