0b844c97399fabbf4a4775e7da449c799faea5a8
Namito Yokota <namitoyokota@icloud.com>
2024-03-08 14:24:09 -0600

    use reveal wrapper

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { RevealWrapper } from 'next-reveal';
 import Layout from '../components/Layout';
 import Notation from '../components/Notaion';
 import { NotationConfigs } from '../models/constants/notation-config';
@@ -8,42 +9,50 @@ export const Index = (): JSX.Element => {
         <Layout>
             <div className={styles.pane}>
                 {/* first impression */}
-                <div className={styles.header}>
-                    <h1>
-                        hey, i'm namito <span className="animate__animated animate__bounce">👋🏼</span>
-                    </h1>
-                    <span>
-                        i write <Notation config={NotationConfigs.underline}>clean code</Notation> with a smile.
-                    </span>
-                </div>
+                <RevealWrapper>
+                    <div className={styles.header}>
+                        <h1>
+                            hey, i'm namito <span className="animate__animated animate__bounce">👋🏼</span>
+                        </h1>
+                        <span>
+                            i write <Notation config={NotationConfigs.underline}>clean code</Notation> with a smile.
+                        </span>
+                    </div>
+                </RevealWrapper>
 
                 {/* about me */}
-                <section>
-                    <h3>about me</h3>
-                    <p>
-                        i am a <u>frontend developer</u>.
-                    </p>
-                </section>
+                <RevealWrapper>
+                    <section>
+                        <h3>about me</h3>
+                        <p>
+                            i am a <u>frontend developer</u>.
+                        </p>
+                    </section>
+                </RevealWrapper>
 
                 {/* my story */}
-                <section>
-                    <h3>my story</h3>
-                    <p>
-                        when i was in <u>9th grade</u>, a business class i wanted to sign up for was full. my counselor told me that i can
-                        either take "coding" or "piano" class. ever since i made the obvious choice, i've been in love with the{' '}
-                        <u>freedom and creativity</u> of coding!
-                    </p>
-                </section>
+                <RevealWrapper>
+                    <section>
+                        <h3>my story</h3>
+                        <p>
+                            when i was in <u>9th grade</u>, a business class i wanted to sign up for was full. my counselor told me that i
+                            can either take "coding" or "piano" class. ever since i made the obvious choice, i've been in love with the{' '}
+                            <u>freedom and creativity</u> of coding!
+                        </p>
+                    </section>
+                </RevealWrapper>
 
                 {/* action step */}
-                <section>
-                    <h3>what's next?</h3>
-                    <p>
-                        i'm open to contributing as a senior frontend developer at a{' '}
-                        <Notation config={NotationConfigs.underline}>passionate</Notation> and{' '}
-                        <Notation config={NotationConfigs.underline}>team-oriented</Notation> organization!
-                    </p>
-                </section>
+                <RevealWrapper>
+                    <section>
+                        <h3>what's next?</h3>
+                        <p>
+                            i'm open to contributing as a senior frontend developer at a{' '}
+                            <Notation config={NotationConfigs.underline}>passionate</Notation> and{' '}
+                            <Notation config={NotationConfigs.underline}>team-oriented</Notation> organization!
+                        </p>
+                    </section>
+                </RevealWrapper>
 
                 {/* TODO: Coding languages I'm comfortable with */}
 
