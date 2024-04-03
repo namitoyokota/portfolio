bb3e3498a12b241648e831d901a2a7772b60f533
Namito Yokota <namitoyokota@icloud.com>
2024-04-02 22:52:37 -0500

    remove resume from footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,11 +20,6 @@ const Footer = (): JSX.Element => {
                     <Image alt="link" height="12" width="12" src="/icons/link.svg" />
                 </a>
 
-                <a className="sentence" href="https://api.namitoyokota.com/assets/documents/resume.pdf" target="_blank" rel="noreferrer">
-                    <span>Resume</span>
-                    <Image alt="link" height="12" width="12" src="/icons/link.svg" />
-                </a>
-
                 <a className="sentence" href="mailto:namitoyokota@icloud.com">
                     <span>Email</span>
                     <Image alt="link" height="12" width="12" src="/icons/link.svg" />
