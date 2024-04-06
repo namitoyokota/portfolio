0217eee05fd02e37e727dbd032d2ddc4dd95e0a9
Namito Yokota <namitoyokota@icloud.com>
2024-04-05 22:36:20 -0500

    constants/interfaces clean up

--- models/interface/post.ts
+++ models/interface/post.ts
@@ -1,8 +1,23 @@
+/**
+ * Represents a blog post from Hashnode
+ * API documentation: https://gql.hashnode.com/?source=legacy-api-page#
+ */
 export interface Post {
+    /** The ID of the post. Used to uniquely identify the post. */
     id: string;
+
+    /** Complete URL of the post including the domain name. Example - https://johndoe.com/my-post-slug */
     url: string;
+
+    /** The title of the post. */
     title: string;
+
+    /** The date and time the post was published. */
     publishedAt: string;
+
+    /** The subtitle of the post. Subtitle is a short description of the post which is also used in SEO if meta tags are not provided. */
     subtitle: string;
+
+    /** The number of hearts on the post. Shows how many users liked the post. */
     reactionCount: number;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/legacy/image';
+import { RoughAnnotationConfig } from 'rough-notation/lib/model';
 import Label from '../components/Label';
 import Layout from '../components/Layout';
 import Notation from '../components/Notaion';
-import { NotationConfigs } from '../models/constants/notation-config';
 import styles from '../styles/index.module.css';
 
 export const Index = (): JSX.Element => {
+    /** Configuration for rough notation */
+    const underline = {
+        type: 'underline',
+        color: '#f97316',
+    } as RoughAnnotationConfig;
+
     return (
         <Layout
             customMeta={{
@@ -87,26 +93,25 @@ export const Index = (): JSX.Element => {
                 </label>
                 <div className={styles.list}>
                     <span>
-                        At <Notation config={NotationConfigs.underline}>Venminder</Notation>, I have been leading the front-end development
-                        of a web application generating 60% of company revenue. I love my experience so far here as I've been trusted with
-                        the freedom to explore new libraries and create prototypes.
+                        At <Notation config={underline}>Venminder</Notation>, I have been leading the front-end development of a web
+                        application generating 60% of company revenue. I love my experience so far here as I've been trusted with the
+                        freedom to explore new libraries and create prototypes.
                     </span>
 
                     <span>
                         Before Venminder, I spent 2 years developing a highly complex public safety web product at{' '}
-                        <Notation config={NotationConfigs.underline}>Hexagon</Notation>. Working on this team was one of my favorite
-                        experiences due to it's advancement in the codebase. We had a micro-frontend architecture where over 40 npm packages
-                        were communicating with each other without a circular dependency which then is consumed by the root repository to
-                        serve the main site.
+                        <Notation config={underline}>Hexagon</Notation>. Working on this team was one of my favorite experiences due to it's
+                        advancement in the codebase. We had a micro-frontend architecture where over 40 npm packages were communicating with
+                        each other without a circular dependency which then is consumed by the root repository to serve the main site.
                     </span>
 
                     <span>
                         Prior to Hexagon, I worked as an intern with 2 different companies: threeDays and Trips4Trade. At{' '}
-                        <Notation config={NotationConfigs.underline}>threeDays</Notation>, I was able to learn the basics of creating static
-                        websites. This internship helped me understand the importance of managing a clean codebase. At{' '}
-                        <Notation config={NotationConfigs.underline}>Trips4Trade</Notation> however, I learned a new set of tools with using
-                        WordPress. I gained experience pushing changes up to different environments, refactoring existing databases, as well
-                        as using third-party plugins.
+                        <Notation config={underline}>threeDays</Notation>, I was able to learn the basics of creating static websites. This
+                        internship helped me understand the importance of managing a clean codebase. At{' '}
+                        <Notation config={underline}>Trips4Trade</Notation> however, I learned a new set of tools with using WordPress. I
+                        gained experience pushing changes up to different environments, refactoring existing databases, as well as using
+                        third-party plugins.
                     </span>
                 </div>
             </section>
@@ -188,9 +193,8 @@ export const Index = (): JSX.Element => {
                             </div>
                             <label>Undergraduate degree</label>
                             <span>
-                                I have a degree from the University of Alabama in{' '}
-                                <Notation config={NotationConfigs.underline}>Computer Science</Notation> with a{' '}
-                                <Notation config={NotationConfigs.underline}>Mathematics</Notation> minor. Some of my favorite courses were:
+                                I have a degree from the University of Alabama in <Notation config={underline}>Computer Science</Notation>{' '}
+                                with a <Notation config={underline}>Mathematics</Notation> minor. Some of my favorite courses were:
                             </span>
                             <div className="items">
                                 <div className="bullet">
