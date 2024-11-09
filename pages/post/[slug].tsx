11b4d9d21ea24de4724b17d6c3946b5c8fd5d5a9
Namito Yokota <namitoyokota@icloud.com>
2024-11-08 20:13:53 -0800

    Add loading message to post page.

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,70 +1,82 @@
+'use client';
 import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
 import { format, parseISO } from 'date-fns';
+import { useEffect, useState } from 'react';
 import Label from '../../components/Label';
 import Layout from '../../components/Layout';
-import { PostProps } from '../../models/types/post-props';
+import { Post } from '../../models/interface/post';
 import styles from '../../styles/post.module.css';
 
-export const PostPage = ({ post }: PostProps): JSX.Element => {
-    return (
-        <Layout
-            customMeta={{
-                title: post.title,
-                description: post.subtitle,
-            }}
-        >
-            <div className={styles.content}>
-                <h1>{post.title}</h1>
-                <span className="grey">{post.subtitle}</span>
-                <div className={styles.menu}>
-                    <Label alternate={true}>{format(parseISO(post.publishedAt!), 'MMMM dd, yyyy')}</Label>
-                    <Label>
-                        <a href={post.url} target="_blank" rel="noopener noreferrer">
-                            View Original
-                        </a>
-                    </Label>
-                </div>
+export const PostPage = ({ props }): JSX.Element => {
+    const [post, setPost] = useState<Post>(null);
 
-                <article>
-                    <div dangerouslySetInnerHTML={{ __html: post.content.html }}></div>
-                </article>
-            </div>
-        </Layout>
-    );
-};
+    const [loading, setLoading] = useState(true);
 
-export async function getServerSideProps({ params }) {
-    const client = new ApolloClient({
-        uri: 'https://gql.hashnode.com/',
-        cache: new InMemoryCache(),
-    });
+    useEffect(() => {
+        const slug = window.location.pathname.split('/')[2];
+        const client = new ApolloClient({
+            uri: 'https://gql.hashnode.com/',
+            cache: new InMemoryCache(),
+        });
 
-    const { data } = await client.query({
-        query: gql`
-                query Publication {
-                    publication(host: "blogs.namitoyokota.com") {
-                        post(slug: "${params.slug}") {
-                            id
-                            url
-                            title
-                            subtitle
-                            publishedAt
-                            reactionCount
-                            slug
-                            content {
-                                html
+        client
+            .query({
+                query: gql`
+                    query Publication {
+                        publication(host: "blogs.namitoyokota.com") {
+                            post(slug: "${slug}") {
+                                id
+                                url
+                                title
+                                subtitle
+                                publishedAt
+                                reactionCount
+                                slug
+                                content {
+                                    html
+                                }
                             }
                         }
                     }
-                }
-            `,
-    });
+                `,
+            })
+            .then((response) => setPost(response.data.publication.post))
+            .finally(() => setLoading(false));
+    }, []);
 
-    return {
-        props: {
-            post: data.publication.post,
-        },
-    };
-}
+    return (
+        <>
+            {loading ? (
+                <Layout>
+                    <h1>Loading...</h1>
+                </Layout>
+            ) : (
+                <Layout
+                    customMeta={{
+                        title: post.title,
+                        description: post.subtitle,
+                    }}
+                >
+                    <div className={styles.content}>
+                        <h1>{post.title}</h1>
+                        <span className="grey">{post.subtitle}</span>
+                        <div className={styles.menu}>
+                            <Label alternate={true}>{format(parseISO(post.publishedAt!), 'MMMM dd, yyyy')}</Label>
+                            <Label>
+                                <a href={post.url} target="_blank" rel="noopener noreferrer">
+                                    View Original
+                                </a>
+                            </Label>
+                        </div>
+
+                        <article>
+                            <div dangerouslySetInnerHTML={{ __html: post.content.html }}></div>
+                        </article>
+                    </div>
+                </Layout>
+            )}
+        </>
+    );
+};
 
 export default PostPage;
