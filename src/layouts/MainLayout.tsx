86d643890bd964cf0f34cf8efe2fd8ab67bddfbc
Namito Yokota <namitoyokota@icloud.com>
2025-04-12 22:15:56 -0500

    Remove projects page.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,7 @@
 import { HomeModule } from '@/modules/home';
 import { PostsModule } from '@/modules/posts';
-import { ProjectsModule } from '@/modules/projects';
 import { WorkModule } from '@/modules/work';
-import { faBook, faBriefcase, faFolder, faHome } from '@fortawesome/free-solid-svg-icons';
+import { faBook, faBriefcase, faHome } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link, Route, Routes } from 'react-router';
 
@@ -10,7 +9,7 @@ export const MainLayout = () => {
   return (
     <div className="flex items-center justify-center">
       <div className="flex max-w-[750px] flex-col items-start gap-4 p-8 text-gray-700">
-        <nav className="flex flex-wrap items-center justify-between gap-x-4 gap-y-2">
+        <nav className="flex flex-wrap items-center gap-x-4 gap-y-2">
           <Link to="/" className="flex items-center gap-2 decoration-2 hover:underline">
             <FontAwesomeIcon icon={faHome} size="sm" />
             <span>Home</span>
@@ -18,10 +17,6 @@ export const MainLayout = () => {
           <Link to="/work" className="flex items-center gap-2 decoration-2 hover:underline">
             <FontAwesomeIcon icon={faBriefcase} size="sm" />
             <span>Work</span>
-          </Link>
-          <Link to="/projects" className="flex items-center gap-2 decoration-2 hover:underline">
-            <FontAwesomeIcon icon={faFolder} size="sm" />
-            <span>Projects</span>
           </Link>
           <Link to="/posts" className="flex items-center gap-2 decoration-2 hover:underline">
             <FontAwesomeIcon icon={faBook} size="sm" />
@@ -33,7 +28,6 @@ export const MainLayout = () => {
           <Routes>
             <Route path="*" element={<HomeModule />} />
             <Route path="/work/*" element={<WorkModule />} />
-            <Route path="/projects/*" element={<ProjectsModule />} />
             <Route path="/posts/*" element={<PostsModule />} />
           </Routes>
         </div>
