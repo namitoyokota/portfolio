f3592b7e3ed8940fe2f5e930ada67255d7177f1e
Namito Yokota <namitoyokota@icloud.com>
2025-04-15 16:40:42 -0500

    Replace email link underline with rough notation.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,3 +1,4 @@
+import { RevealNotation } from '@/components/RevealNotation';
 import { HomeModule } from '@/modules/home';
 import { PostsModule } from '@/modules/posts';
 import { WorkModule } from '@/modules/work';
@@ -43,9 +44,9 @@ export const MainLayout = () => {
 
         <p>
           For any inquiries,{' '}
-          <a href="mailto:namitoyokota@icloud.com" className="underline decoration-2">
-            send me an email!
-          </a>
+          <RevealNotation>
+            <a href="mailto:namitoyokota@icloud.com">send me an email!</a>
+          </RevealNotation>
         </p>
       </div>
     </div>
