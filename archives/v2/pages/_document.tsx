5f054625d21416fcafc50f081771e396003250ff
Namito Yokota <namitoyokota@icloud.com>
2025-04-17 17:07:05 -0500

    Add archived messages to deprecated projects.

--- archives/v2/pages/_document.tsx
+++ archives/v2/pages/_document.tsx
@@ -6,6 +6,39 @@ class MyDocument extends Document {
             <Html lang="en">
                 <Head />
                 <body>
+                    <div
+                        style={{
+                            display: 'flex',
+                            gap: '0.5rem',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            padding: '2rem',
+                        }}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        >
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+                            <path d="M12 8v4" />
+                            <path d="M12 16h.01" />
+                        </svg>
+                        <span>
+                            This site has been archived. View the latest version{' '}
+                            <a href="https://www.namitoyokota.com" target="_blank" rel="noreferrer">
+                                here
+                            </a>
+                            !
+                        </span>
+                    </div>
                     <Main />
                     <NextScript />
                 </body>
