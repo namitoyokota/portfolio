7e2714e89042524be23d4027a4d8f63f046eefe8
Namito Yokota <namitoyokota@icloud.com>
2025-04-18 09:58:32 -0500

    Remove rough-notation.

--- src/modules/posts/components/NoteItem.tsx
+++ src/modules/posts/components/NoteItem.tsx
@@ -1,5 +1,4 @@
 import { RevealCard } from '@/components/RevealCard';
-import { RevealNotation } from '@/components/RevealNotation';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Note } from '../lib/note';
@@ -9,20 +8,16 @@ interface NoteItemProps {
 }
 
 export const NoteItem = ({ note }: NoteItemProps) => {
-  const renderNoteLink = () => (
-    <a href={note.url} target="_blank" rel="noreferrer">
-      {note.title}
-    </a>
-  );
-
   return (
     <RevealCard>
       <li className="flex items-center gap-3 py-1">
         <div className="flex items-center rounded-md bg-gray-200 px-1.5 py-1 text-gray-600">
           <FontAwesomeIcon icon={faArrowRight} className="rotate-315" />
         </div>
-        <div className="flex flex-col gap-1">
-          <div>{note.reactionCount > 0 ? <RevealNotation>{renderNoteLink()}</RevealNotation> : renderNoteLink()}</div>
+        <div className="flex flex-col">
+          <a href={note.url} target="_blank" rel="noreferrer">
+            {note.title}
+          </a>
           <span className="text-sm text-gray-400">{note.subtitle}</span>
         </div>
       </li>
--- src/modules/posts/components/PostItem.tsx
+++ src/modules/posts/components/PostItem.tsx
@@ -1,5 +1,4 @@
 import { RevealCard } from '@/components/RevealCard';
-import { RevealNotation } from '@/components/RevealNotation';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Post } from '../lib/post';
@@ -9,19 +8,15 @@ interface PostItemProps {
 }
 
 export const PostItem = ({ post }: PostItemProps) => {
-  const renderPostLink = () => (
-    <a href={post.url} target="_blank" rel="noreferrer" className="font-semibold">
-      {post.title}
-    </a>
-  );
-
   return (
     <RevealCard>
       <li className="flex items-center gap-3 py-1">
         <div className="flex items-center rounded-md bg-gray-200 px-1.5 py-1 text-gray-600">
           <FontAwesomeIcon icon={faArrowRight} className="rotate-315" />
         </div>
-        {post.reactionCount > 0 ? <RevealNotation>{renderPostLink()}</RevealNotation> : renderPostLink()}
+        <a href={post.url} target="_blank" rel="noreferrer" className="font-semibold">
+          {post.title}
+        </a>
         <span className="hidden text-sm text-gray-400 sm:block">{new Date(post.publishedAt).toLocaleDateString()}</span>
       </li>
     </RevealCard>
