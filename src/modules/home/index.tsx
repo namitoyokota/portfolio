e324884df7f4581a6f8fc41bec21a1700c5e0ddf
Namito Yokota <namitoyokota@icloud.com>
2025-04-30 12:20:36 -0500

    Reset home page title.

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,8 +1,15 @@
 import { NotFound } from '@/components/NotFound';
-import { Route, Routes } from 'react-router';
+import { useEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router';
 import { Home } from './components/Home';
 
 export const HomeModule = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    document.title = 'Namito Yokota';
+  }, [location.pathname]);
+
   return (
     <Routes>
       <Route index element={<Home />} />
