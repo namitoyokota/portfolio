08d5f7e2653bb537e186db07510e8d1e6cfd4ef1
Namito Yokota <namitoyokota@icloud.com>
2025-07-07 17:19:21 -0500

    Install vite-plugin-capo.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import tailwindcss from '@tailwindcss/vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { capo } from 'vite-plugin-capo';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss(), tsconfigPaths(), basicSsl()],
+    plugins: [react(), tailwindcss(), tsconfigPaths(), basicSsl(), capo()],
     publicDir: 'public',
   };
 });
